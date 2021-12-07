62f4beb5eb3f8d76be9629a2cc775a3d1bf4d24e
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-12-07 08:13:20 -0800

    start of custom component build out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
-     
+     <h1>Proof of Life!</h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
