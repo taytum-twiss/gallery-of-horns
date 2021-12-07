d27e1f6db8fad53f926272621c595135cd7d50b3
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-12-07 08:33:31 -0800

    header component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from '.Header';
 
 class App extends React.Component {
   render() {
     return (
     <div className="App">
      <h1>Proof of Life!</h1>
+     <Header />
     </div>
     );
   }
