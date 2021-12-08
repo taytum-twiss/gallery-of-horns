0c88cd7eceb31b678e5ed8d48882a05c8c1773f8
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-12-08 08:20:03 -0800

    created HornedBeast componet

---
+++ src/HornedBeast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+    render() {
+        return (
+     <div>
+      <h2>Beast Title coming soon...</h2>
+      <img
+       src='https://via.placeholder.com/150'
+        alt='alt coming soon...' 
+        title='title coming soon...' 
+        style={{ width: "20%" }} 
+        />
+      <p>Beast Description coming soon...</p>
+     </div>
+
+        )
+    }
+}
+
+export default HornedBeast;
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HornedBeast from './HornedBeast.js';
 
 class Main extends React.Component {
     render() {
         return (
+           <> 
             <p>Horned Beasts coming soon...</p>
+            <HornedBeast />
+           </> 
         )
     }
 }
