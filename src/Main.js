27d47ccf50540e3f18a0293ab781cd881e04aad5
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-12-09 08:18:36 -0800

    applied bootstrap container to main component

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
 import beastData from'./beastData.json';
+import Container from "react-bootstrap/Container";
+
 
 class Main extends React.Component {
     render() {
         return (
-           <> 
+           <Container> 
             <p>Horned Beasts coming soon...</p>
             {beastData.map((currentValueBeast, idx) => (
             <HornedBeast key={idx} beastObj={currentValueBeast} />
             ))}
-           </> 
+           </Container> 
         )
     }
 }
