ebc25b7cf4c289fd00c039ff7e925481b293b6ea
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-12-13 08:51:43 -0800

    worked on take home assignment

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 class FilterForm extends React.Component {
     render() {
         return (
             <>
-                <form className="form">
-                <label for="numOfHorns">Filter by Horns</label>
+                <Form className="form">
+                <Form.Label for="numOfHorns">Filter by Horns</Form.Label>
                 <br/>
-                <select className="formSelect" name="numOfHorns">
+                <Form.Select className="formSelect" name="numOfHorns">
                  <option>All Beasts</option>
                  <option>1 Horn</option>
                  <option>2 Horns</option>
                  <option>3 Horns</option>
                  <option>100 Horns</option>
-                </select>
-               </form>
+                </Form.Select>
+               </Form>
             </>
         )
     }
